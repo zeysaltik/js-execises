1c20cd06cb61f5de79cf50e2f9c39b2a4fb24a8e
zeysaltik <zeysaltik200@gmail.com>
2024-07-08 17:03:48 +0300

    degisken.js add

---
+++ degisken.js
@@ -0,0 +1,38 @@
+let name = "Zeynep"
+let isActive = true
+let myUndefined = undefined
+let myNull = null
+
+console.log(typeof (name))
+console.log(typeof (isActive))
+console.log(typeof (myUndefined))
+console.log(typeof (myNull))
+
+let degisken1;
+let degisken2;
+let degisken3;
+let degisken4;
+
+degisken1 = "Zeynep"
+degisken2 = "Deniz"
+degisken3 = "Saniye"
+degisken4 = "Mustafa"
+
+console.log(degisken1)
+console.log(degisken2)
+console.log(degisken3)
+console.log(degisken4)
+
+let myName = "Zeynep"
+let mySurname = "Saltık"
+let maritalStatus = "Bekar"
+let country = "Türkiye"
+let age = 16
+
+let myName1 = "Zeynep", mySurname1 = "Saltık", maritalStatus1 = "Bekar", country1 = "Turkiye", age1 = 16;
+
+let benimYasim = "Ben 25 Yaşındayım."
+let seninYasin = "Sen 30 Yaşındasın."
+
+console.log(benimYasim)
+console.log(seninYasin)

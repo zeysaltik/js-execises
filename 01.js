9fbe8194289d6017c38a61baa4c9f8a95c561e2b
zeysaltik <zeysaltik200@gmail.com>
2024-07-08 16:58:40 +0300

    01.js add

---
+++ 01.js
@@ -0,0 +1,12 @@
+
+
+//Yorumlar kodu okunabilir hale getirmeye yardımcı olur
+
+
+// 30GundeJavaScript eğitimine hoş geldiniz
+
+/*
+Yorumlar kodu okunabilir, 
+yeniden kullanımı kolay ve 
+bilgilendirici bir hale getirir.
+*/
